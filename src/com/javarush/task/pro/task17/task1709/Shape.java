2e9e48f7cf586d9da09854296ae141e40ba0b77d
Salovkusno (Salovkusno) <kostya_vasyukov@mail.ru>
2024-04-04 02:48:29 +0300

    первая фиксация

---
+++ src/com/javarush/task/pro/task17/task1709/Shape.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task17.task1709;
+
+/* 
+Абстрактные фигуры
+*/
+
+public abstract class Shape {
+
+    abstract double getPerimeter();
+    abstract double getArea();
+
+
+    //напишите тут ваш код
+}
